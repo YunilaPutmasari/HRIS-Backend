88855c07c8933875fe48e900263fce61fe72c642
hoshigakikisame <rianatgithub@gmail.com>
2025-05-20 05:02:29 +0700

    feat: attendance models

---
+++ app/Models/Attendance/CheckClockSetting.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models\Attendance;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+
+class CheckClockSetting extends Model
+{
+    protected $table = "tb_check_clock_setting";
+    protected $primaryKey = 'id';
+
+    protected $with = [
+        'checkClockSettingTime',
+    ];
+
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'id_company',
+        'type'
+    ];
+
+    public function checkClockSettingTime()
+    {
+        return $this->hasMany(CheckClockSettingTime::class, 'id_check_clock_setting');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class, 'id_company');
+    }
+}
---
+++ app/Models/Attendance/CheckClockSettingTime.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models\Attendance;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+
+class CheckClockSettingTime extends Model
+{
+    protected $table = "tb_check_clock_setting_time";
+    protected $primaryKey = 'id';
+
+    protected $with = [
+        'checkClockSetting',
+    ];
+
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $fillable = [
+        'id_check_clock_setting',
+        'day',
+        'clock_in',
+        'clock_out',
+        'break_start',
+        'break_end',
+    ];
+
+    public function checkClockSetting()
+    {
+        return $this->hasMany(CheckClockSetting::class, 'id_check_clock_setting');
+    }
+}
